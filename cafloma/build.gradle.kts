08a80443b521cda08156b16ded1add26bf560da2
Rene Sommerfeld <140407568+reen28@users.noreply.github.com>
2024-10-18 23:35:55 +0200

    setup(spring-boot-kotlin): setup spring boot project with kotlin (closes #5) (#7)

---
+++ cafloma/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	// kotlin
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+
+	// versions
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "de.reen28"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	//kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// spring boot core dependencies
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// testing dependencies
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
