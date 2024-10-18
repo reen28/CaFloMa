5f0066e87da5062770e31a991906dae8f713c204
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2024-10-18 22:31:31 +0200

    setup(spring-boot-kotlin): setup spring boot project with kotlin (closes #5)

---
+++ cafloma/src/main/kotlin/de/reen28/cafloma/CaflomaApplication.kt
@@ -0,0 +1,11 @@
+package de.reen28.cafloma
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaflomaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaflomaApplication>(*args)
+}
