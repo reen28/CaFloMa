bdfa283c14ff9f94cad2e44f5bb5f8aa2056a62d
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2024-10-20 22:44:10 +0200

    refactor(amount): changing currency field from type Currency to String

--- cafloma/src/main/kotlin/de/reen28/cafloma/internal/database/Amount.kt
+++ cafloma/src/main/kotlin/de/reen28/cafloma/internal/database/Amount.kt
@@ -1,10 +1,9 @@
 package de.reen28.cafloma.internal.database
 
 import jakarta.persistence.Embeddable
-import java.util.Currency
 
 @Embeddable
 data class Amount(
     val value: Long,
-    val currency: Currency
+    val currency: String
 )
